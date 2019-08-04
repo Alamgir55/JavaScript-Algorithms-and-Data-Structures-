43deb7d221cd404c75c59880bc6f5640e9950ebe
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-04 18:19:44 -0400

    initial commit

---
+++ Big O Notation/scripts.js
@@ -0,0 +1,61 @@
+// Bad
+function addUpTo(n){
+    let total = 0;
+    for(let i = 1; i <= n; i++){
+        total += i;
+    }
+    return total;
+}
+//console.log(addUpTo(6));
+
+// Good
+function addUpTo2(n){
+    return n * (n + 1) / 2;
+}
+//console.log(addUpTo2(2));
+
+// Bad 
+function addUpToTime(n){
+    let total = 0;
+    for(let i = 1; i <= n; i++){
+        total += i;
+    }
+    return total;
+}
+let t1 = performance.now();
+//addUpToTime(100000000);
+let t2 = performance.now();
+//console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds `);
+
+// Good 
+
+function addUpToTime2(n){
+    return n * (n + 1) / 2;
+}
+let time1 = performance.now();
+//addUpToTime2(100000000);
+let time2 = performance.now();
+//console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds `);
+
+////
+function countUpAndDown(n){
+    console.log('Going up!');
+    for(let i = 0; i < n; i++){
+        console.log(i);
+    }
+    console.log('At the top!\nGoing down..');
+    for(let j = n - 1; j >= 0; j--){
+        console.log(j);
+    }
+    console.log("Break down.By");
+}
+//countUpAndDown(10);
+
+function printAllPairs(n){
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++){
+            console.log(i, j);
+        }
+    }
+}
+//printAllPairs(2);
