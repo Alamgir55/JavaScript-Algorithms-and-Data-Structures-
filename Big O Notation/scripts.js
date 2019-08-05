9d888cf019d33366fef573ee9dc6a51b9d4e11bf
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-05 18:39:19 -0400

    initial commit

--- Big O Notation/scripts.js
+++ Big O Notation/scripts.js
@@ -59,3 +59,37 @@ function printAllPairs(n){
     }
 }
 //printAllPairs(2);
+
+function logAtLeast5(n){
+    for(let i = 1; i <= Math.max(5, n); i++){
+        console.log(i);
+    }
+}
+
+//logAtLeast5(1);
+
+function logAtMost5(n){
+    for(let i = 1; i <= Math.min(5,n); i++){
+        console.log(i);
+    }
+}
+//logAtMost5();
+
+function sum(arr){
+    let total = 0;
+    for(let i = 0; i < arr.length; i++){
+        total += arr[i];
+    }
+    return total;
+}
+//console.log(sum([10,2]));
+
+function double(arr){
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(2 * arr[i]);
+    }
+    return newArr;
+}
+
+console.log(double([1, 2, 3]));
