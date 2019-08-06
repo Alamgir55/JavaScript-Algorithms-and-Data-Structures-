a9fdfcfe2634f8c17a6000531aada4d91777d881
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-06 19:30:31 -0400

    initial commit

---
+++ 2.Analyzing Performance of Arrays and Objects/scripts.js
@@ -0,0 +1,17 @@
+let instructor = {
+    firstName: "kelly",
+    isInstructor: true,
+    favoriteName: [1,2,3,4]
+}
+
+// // O(n)
+// console.log(Object.keys(instructor));
+
+// //O(n)
+// console.log(Object.values(instructor));
+
+// //O(n)
+// console.log(Object.entries(instructor));
+
+// // O(1)
+// console.log(instructor.hasOwnProperty('firstName'));
