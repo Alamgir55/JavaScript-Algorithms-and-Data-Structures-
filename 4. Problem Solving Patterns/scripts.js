cf057d875ae58cfadc4ee8d854ef9cb1e4acf438
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-13 19:56:25 -0400

    initial commit

--- 4. Problem Solving Patterns/scripts.js
+++ 4. Problem Solving Patterns/scripts.js
@@ -77,7 +77,7 @@ same([1,2,3,2], [9,1,4,4]); */
 
 str('rakib', 'kibra'); */
 
-function validAnagram(first, second){
+/* function validAnagram(first, second){
     if(first.length !== second.length){
         return false;
     }
@@ -99,4 +99,52 @@ function validAnagram(first, second){
     return true;
 }
 
-validAnagram('anagram', 'nagaram');
+validAnagram('anagram', 'nagaram'); */
+
+// Bad Code
+
+/* function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        console.log(i);
+        for(let j = i+1; j < arr.length; j++){
+            
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+console.log(sumZero([-4,-3,-2,-1,0,1,2,5])); */
+
+// Good Code
+
+/* function sumZero(arr){
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]];
+        }else if(sum > 0){
+            right--;
+        }else{
+            left++;
+        }
+    }
+}
+
+console.log(sumZero([-4,-3,-2,-1,0,1,2,3,10])); */
+
+/* function countUniqueValue(arr){
+    let i = 0;
+    for(let j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1;
+}
+console.log(countUniqueValue([1,1,1,2,3,5,4,1])); */
+
