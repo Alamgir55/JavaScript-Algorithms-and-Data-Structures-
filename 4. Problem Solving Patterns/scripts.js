78dea01b72c9a3176236e3b5b858a6fafd089bfc
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-14 20:20:39 -0400

    initial commit

--- 4. Problem Solving Patterns/scripts.js
+++ 4. Problem Solving Patterns/scripts.js
@@ -148,3 +148,41 @@ console.log(sumZero([-4,-3,-2,-1,0,1,2,3,10])); */
 }
 console.log(countUniqueValue([1,1,1,2,3,5,4,1])); */
 
+// Bad Code
+/* function maxSubarraySum(arr, num){
+    if(num > arr.length){
+        return null;
+    }
+    var max = -Infinity;
+    for(let i = 0; i < arr.length - num + 1; i ++){
+        temp = 0;
+        for(let j = 0; j < num; j++){
+            temp += arr[i + j];
+        }
+        if(temp > max){
+            max = temp;
+        }
+        console.log(temp, max);
+    }
+    return max;
+}
+maxSubarraySum([2,6,9,2,1,8,5,6,3],3); */
+
+function maxSubarraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    for(let i = 0; i < num; i++){
+        maxSum += arr[i]; 
+    }
+    temSum = maxSum;
+    console.log(tempSum);
+    for(let i = num; i < arr.length; i++){
+        console.log(i)
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    //return maxSum;
+    console.log(tempSum,maxSum);
+}
+maxSubarraySum([2,6,9,2,1,8,5,6,3],3);
