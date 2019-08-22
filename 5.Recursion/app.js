41c12c22297bb8a82ecd00ff7729c496e6148d1a
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-22 19:29:25 -0400

    initial commit

--- 5.Recursion/app.js
+++ 5.Recursion/app.js
@@ -50,5 +50,21 @@ console.log(factorial(4));      */
             
 console.log(factorial(4)); */
 
-        
+
+function collecctOddValues(arr){
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0){
+            return;
+        }
+
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+
+        helper(helperInput.slice(1));
+        helper(arr);
+    }
+}
                            
