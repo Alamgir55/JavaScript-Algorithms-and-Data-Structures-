03c98be5ec11180008fe74ecf3ab12445288a12f
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-21 21:11:40 -0400

    initial commit

---
+++ 5.Recursion/app.js
@@ -0,0 +1,54 @@
+// Recursive Function
+
+/* function countDown(num){
+    if(num <= 0){
+        console.log("All Done");
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+} */
+//countDown(3);
+
+//Non-Recursive Function
+
+/* function countDown(num){
+    for(var i = num; i > 0; i--){
+            console.log(i);
+    }
+    console.log("All done");
+}
+countDown(5); */
+
+// Recursive Function
+
+// function sumRange(num){
+//     if(num === 1) return 1;
+//     return num + sumRange(num-1);
+// }
+
+// console.log(sumRange(5));
+
+    //Non-Recursive Function
+/* function factorial(num){
+    let total = 1;
+    for(let i = num; i > 0; i--){
+        console.log(i);
+        console.log(total); 
+        total *= i
+    }
+    return total;
+}           
+console.log(factorial(4));      */           
+   
+    // Recursive Function
+/* function factorial(num){
+    if(num === 1) return 1;
+    return num * factorial(num-1);
+}                        
+            
+console.log(factorial(4)); */
+
+        
+                           
