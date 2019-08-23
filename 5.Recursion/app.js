72794cf0457e1a4823ce47d11b959b260938fccb
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-22 21:52:37 -0400

    initial commit

--- 5.Recursion/app.js
+++ 5.Recursion/app.js
@@ -50,7 +50,7 @@ console.log(factorial(4));      */
             
 console.log(factorial(4)); */
 
-
+/* 
 function collecctOddValues(arr){
     let result = [];
 
@@ -63,8 +63,64 @@ function collecctOddValues(arr){
             result.push(helperInput[0])
         }
 
-        helper(helperInput.slice(1));
-        helper(arr);
+        helper(helperInput.slice(1))
+    }
+    helper(arr);
+    return result;
+}
+
+console.log(collecctOddValues([1,2,3,4,5,6,7,8,9])); */
+
+/* function collectOddValues(arr){
+    let newArr = [];
+
+    if(arr.length === 0){
+        return newArr;
+    }   
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+console.log(collectOddValues([1,2,3,4,5])); */
+       
+/* function power(str1, str2){
+    if(str1 > 0 || str2 > 0){
+        return Math.pow(str1, str2);
     }
 }
-                           
+
+console.log(power(2, 4)); // 16
+                
+                    */
+                       
+/* function factorial(num){
+    if(num === 0){
+        return 1;
+    }else{
+        return num * factorial(num - 1);
+    }
+
+}
+
+console.log(factorial(4)); */
+
+// function productOfArray(arr) {
+//     if(arr.length === 0){
+//         return;
+//     }
+//     return arr[0] * productOfArray(arr.slice(1));      
+// }   
+
+// productOfArray([1,2,3]);
+
+/* function recursiveRange(num){
+    if(num === 0){
+        return 0;
+    }
+    return num + recursiveRange(num -1);
+    
+}
+console.log(recursiveRange(6)); */
