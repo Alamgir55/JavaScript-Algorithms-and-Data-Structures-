58cf5989754d377cdd53f885c8659d5924f98f92
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-08-24 20:32:40 -0400

    initial commit

--- 5.Recursion/app.js
+++ 5.Recursion/app.js
@@ -124,3 +124,39 @@ console.log(factorial(4)); */
     
 }
 console.log(recursiveRange(6)); */
+
+/* function reverse(str){
+    if(str.length <= 1){
+        return str;
+    }
+    // console.log(str[0])
+    // console.log(str.slice(1))
+    
+    return reverse(str.slice(1)) + str[0];
+}
+
+console.log(reverse('welcome')); */
+
+// function isPalindrome(str){
+//     if(str.length === 1) return true; 
+//     if(str.length === 2) return str[0] === str[1];
+    
+//     if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1));
+//     return false;
+// }
+
+// console.log(isPalindrome('awesome')); // false
+// //isPalindrome('tacocat') // true
+// console.log(isPalindrome('aaaaaaa'));
+
+// someRecursive([4,6,8,9], isOdd) // true
+
+/* const isOdd = val => val % 2 !== 0;
+
+function someRecursive(array, callback){
+    if(array.length === 0) return false;
+    if(callback(array[0])) return true;
+    return someRecursive(array.slice(1), callback);
+}
+
+console.log(someRecursive([4,6,8,9], isOdd)); */
