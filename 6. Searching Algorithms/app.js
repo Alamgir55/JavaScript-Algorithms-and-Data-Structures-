875332020d5f028b7939445caef4bb8bf9c49fcd
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-02 21:44:31 -0400

    initial commit

---
+++ 6. Searching Algorithms/app.js
@@ -0,0 +1,65 @@
+/* let array = [50,11,12,8,6,61,54,41,13];
+
+function findArray(arr, val){
+  for(let i = 0; i < arr.length; i++){
+        if(arr[i] === val){
+            return i;
+        }
+  }
+  return -1;
+   
+}
+
+console.log(findArray(array, 8)); */
+
+// Binary Search 
+/* 
+function binarySearch(arr, elem){
+    let start = 0;
+    let end = arr.length -1;
+    let middle =Math.floor((start + end && start <= end) / 2);
+    console.log(start, middle, end)
+    while(arr[middle] !== elem){
+        
+        if(elem < arr[middle] ){
+            end = middle -1;
+        }else{
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+   if(arr[middle] === elem){
+    return middle;
+   }
+   return -1;
+}
+
+binarySearch([2,3,4,6,8,9,12,15,18,20], 15); */
+              
+
+function binarySearch(arr, val){
+   let start = 0;
+   let end = arr.length - 1;
+   let middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== val){
+        if(val < arr[middle]){
+            start = middle - 1;
+        }else{
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if(arr[middle] === val){
+        return middle;
+    }
+    return -1;
+}
+
+
+console.log(binarySearch([20,26,28,30,34,38,39,40,43,47,49,52,58], 34));
+            // [20,26,28,30,34,38,39,40,43,47,49,52,58]
+            //  s                  m                 e
+                // [20,26,28,30,34,38,39,40,43,47,49,52,58]
+                //                       s      m        e
+                    // [20,26,28,30,34,38,39,40,43,47,49,52,58]
+                    //                                s  m  e
