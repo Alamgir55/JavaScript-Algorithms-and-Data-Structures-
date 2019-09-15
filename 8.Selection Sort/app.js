889ceb89b8ec320539e19957be150716b9e3bb10
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-14 21:12:34 -0400

    initial commit

--- 8.Selection Sort/app.js
+++ 8.Selection Sort/app.js
@@ -1,18 +1,22 @@
 
-function selection(arr){
-   for(let i = 0; i < arr.length; i++){
+
+
+function selectionSort(arr){
+    for(let i = 0; i < arr.length; i++){
         let lowest = i;
         for(let j = i + 1; j < arr.length; j++){
             if(arr[j] < arr[lowest]){
-                lowest = j;  
+                lowest = j;
             }
         }
-        let temp = arr[i];
-        arr[i] = arr[lowest];
-        arr[lowest] = temp;
-   }
-   return arr;
+        if(i !== lowest){
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+    return arr;
 }
 
 
-console.log(selection([34,22,10,19,17]));
+console.log(selectionSort([34,22,10,19,17]));
