7198b559f54bc5db0c9ee2133c92968bca9794ef
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-16 22:55:16 -0400

    initial commit

--- 8.Selection Sort/app.js
+++ 8.Selection Sort/app.js
@@ -1,22 +1,38 @@
 
+// function selectionSort(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         let lowest = i;
+//         for(let j = i + 1; j < arr.length; j++){
+//             if(arr[j] < arr[lowest]){
+//                 lowest = j;
+//             }
+//         }
+//         if(i !== lowest){
+//             let temp = arr[i];
+//             arr[i] = arr[lowest];
+//             arr[lowest] = temp;
+//         }
+//     }
+//     return arr;
+// }
 
 
-function selectionSort(arr){
-    for(let i = 0; i < arr.length; i++){
-        let lowest = i;
-        for(let j = i + 1; j < arr.length; j++){
-            if(arr[j] < arr[lowest]){
-                lowest = j;
-            }
+// console.log(selectionSort([34,22,10,19,17]));
+
+
+
+
+function insertionSort(arr){
+    for(let i = 1; i < arr.length; i++){
+        let currentVal = arr[i];
+        for(var j = i-1; j >= 0 && arr[j] > currentVal; j--){
+            arr[j+1] = arr[j];
         }
-        if(i !== lowest){
-            let temp = arr[i];
-            arr[i] = arr[lowest];
-            arr[lowest] = temp;
-        }
+        arr[j+1] = currentVal;
     }
     return arr;
 }
 
 
-console.log(selectionSort([34,22,10,19,17]));
+
+console.log(insertionSort([2,1,9,4,76]));
