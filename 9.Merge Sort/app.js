37fa1e3cf76b5034c19dada50550a42f93c22dd6
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-19 19:35:16 -0400

    initial commit

---
+++ 9.Merge Sort/app.js
@@ -0,0 +1,27 @@
+
+function marge(arr1, arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            results.push(arr1[i])
+            i++
+        }else{
+            results.push(arr2[j])
+            j++
+        }
+    }
+    while(i < arr1.length){
+        results.push(arr1);
+        i++;
+    }
+    while(j < arr2.length){
+        results.push(arr2);
+        j++;
+    }
+    return results;
+}
+
+
+console.log(marge([1,10,50],[2,14,99,100]));
