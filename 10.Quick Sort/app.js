5da8a891b7d74146c8689569dff6f8fe2e6950ee
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-25 19:57:27 -0400

    initial commit

--- 10.Quick Sort/app.js
+++ 10.Quick Sort/app.js
@@ -14,31 +14,65 @@
 //       if(pivot > arr[i]){
 //         swapIdx++;
 //         swap(arr,swapIdx,i);
-//         console.log(arr)
 //       }
 //     }
 //     swap(arr,start,swapIdx);
-//     console.log(arr)
 //     return swapIdx;
 //   }
-// console.log(pivot([4,8,2,1,5,7,6,3]));
+  
+// //console.log(pivot([4,8,2,1,5,7,6,3]));
+
+// function quickSort(arr, left=0, right = arr.length-1){
+//     if(left < right){
+//         let pivotIndex = pivot(arr, left, right);
+//         // left
+//         quickSort(arr, left, pivotIndex-1);
+//         // right
+//         quickSort(arr, pivotIndex+1, right);
+//     }
+//     return arr;
+// }
+
+// console.log(quickSort([4,8,2,1,5,7,6,3]));
+
+
 
 
 function pivot(arr, start=0, end=arr.length+1){
     function swap(arr, i, j){
         let temp = arr[i];
         arr[i] = arr[j];
-        arr[j] = temp;
+        arr[j] = temp; 
     }
+
     let pivot = arr[start];
-    let swapinx = start;
-    for(let i = pivot + 1; i< arr.length; i++){
-        if(arr[start] > arr[i]){
-            swapinx++;
-            swap(arr, swapinx, i);
+    let swapIdx = start;
+    for(let i = start + 1; i < arr.length; i++){
+        if(pivot > arr[i] ){
+            swapIdx++;
+            swap(arr, swapIdx, i);
         }
     }
-    swap(arr, start, swapinx);
+    swap(arr, start, swapIdx);
+    return swapIdx;
 }
 
-console.log(pivot([4,8,2,1,5,7,6,3]));
+
+function quickSort(arr, left=0, right=arr.length-1){
+
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right);
+        // left
+         quickSort(arr, left, pivotIndex-1);
+        // right
+        quickSort(arr, pivotIndex+1, right);
+    }
+    return arr;
+}
+
+
+
+
+console.log(quickSort([4,8,2,1,5,7,6,3]));
+
+
