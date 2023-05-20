a9f2e551e2b654e7195eafc7543d02391541654b
Alamgir55 <alamgir555hossain@gmail.com>
2023-05-19 23:10:18 -0400

    tree-traversal-BFS-DFSPreOrder-DFSPostOrder-DFSInOrder-

--- 15.Binary search tree/insertTree.js
+++ 15.Binary search tree/insertTree.js
@@ -11,29 +11,29 @@ class BinarySearchTree{
     this.root = null
   }
   insert(value){
-    var newNode = new Node(value)
+    var newNode = new Node(value);
     if(this.root === null){
-      this.root = newNode
-      return this
+        this.root = newNode;
+        return this;
     }
-    var current = this.root
+    var current = this.root;
     while(true){
-      if(value === current.value) return undefined
-      if(value < current.value){
-        if(current.left === null){
-          current.left = newNode
-          return this
+        if(value === current.value) return undefined;
+        if(value < current.value){
+            if(current.left === null){
+                current.left = newNode;
+                return this;
+            }
+            current = current.left;
+        } else {
+            if(current.right === null){
+                current.right = newNode;
+                return this;
+            } 
+            current = current.right;
         }
-        current = current.left
-      }else{
-        if(current.right === null){
-          current.right = newNode
-          return this
-        }
-        current = current.right
-      }
     }
-  }
+}
   
   find(value){
     if(this.root === null) return false
