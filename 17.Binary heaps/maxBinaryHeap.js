f294d3f32c090620ddaaa0a7814f9f408ef22953
Alamgir55 <alamgir555hossain@gmail.com>
2023-05-28 04:40:33 -0400

    extractMax

--- 17.Binary heaps/maxBinaryHeap.js
+++ 17.Binary heaps/maxBinaryHeap.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap{
   constructor(){
-    this.values = [41,39,33,18,27,12]
+    this.values = []
   }
   insert(element){
     this.values.push(element)
@@ -18,7 +18,49 @@ class MaxBinaryHeap{
       idx = parentIdx
     }
   }
+  [12,39,41,33,27]
+  extractMax(){
+    let max = this.values[0]
+    let end = this.values.pop()
+    if(this.values.length > 0){
+      this.values[0] = end
+      this.sinkDown()
+    }
+    return max
+  }
+  sinkDown(){
+   let idx = 0
+   let length = this.values.length()
+   let element = this.values[idx]
+   while(true){
+      let leftChildIdx = 2 * idx + 1
+      let rightChildIdx = 2 * idx + 2
+      let leftChild, rightChild;
+      let swap = null
+      if(leftChildIdx < length){
+        leftChild = this.values[leftChildIdx]
+        if(leftChild > element){
+          swap = leftChild
+        }
+      }
+      if(rightChild < length){
+        rightChild = this.values[rightChildIdx]
+        if((swap == null && rightChild > element) || (swap !== rightChild > leftChild )){
+          swap = rightChild
+        }
+      }
+      if(swap === null) break
+      this.values[idx] = this.values[swap]
+      this.values[swap] = element
+      idx = swap
+   }
+  }
 }
 
 let heap = new MaxBinaryHeap()
 heap.insert(55)
+heap.insert(41)
+heap.insert(39)
+heap.insert(33)
+heap.insert(27)
+heap.insert(12)
